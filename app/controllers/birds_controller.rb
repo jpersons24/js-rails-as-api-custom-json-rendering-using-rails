0115ad7883411c8fc7d11b36e61e4c7e559cb628
Jakob Persons <jakobep24@gmail.com>
2021-01-21 17:02:31 -0500

    walk through

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,18 @@
 class BirdsController < ApplicationController
+
+  # no need for @instance_variables in this case --> rendering 'birds' and 'bird' to JSON instead of using ERB
+
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds, only: [:id, :name, :species]
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      render json: { id: bird.id, name: bird.name, species: bird.species }
+    else
+      render json: { message: 'Bird not found' }
+    end
   end
 end
